87b6c885db65773c29600bd3cbd6dbe8623a843e
Fedor Parenkov <parenkov.fedor@gmail.com>
2022-08-16 15:11:08 +0400

    feat: add class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        let result = Math.round((this.minNum + this.maxNum) / 2);
+        return result;
     }
 
     lower() {
-
+        this.maxNum = Math.round((this.minNum + this.maxNum) / 2);
+        return this.maxNum;
     }
 
     greater() {
-
+        this.minNum = Math.round((this.minNum + this.maxNum) / 2);
+        return this.minNum;
     }
 }
 
